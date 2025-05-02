5012700ee28ea6c132f9347d0fafd2463bd9be3a
manysin <manysin643@gmail.com>
2025-05-02 16:38:41 +0700

    test add true attribute

--- src/components/CustomParseformatWithLang.jsx
+++ src/components/CustomParseformatWithLang.jsx
@@ -10,9 +10,11 @@ export default function CustomParseformatWithLang() {
   const date = "2018 មករា 15";
   // const date = dayjs("2018 Enero 15", "YYYY MMMM DD", "es");
   const formats = ["DD/MM/YYYY h:mm:ss a", "MM-DD-YYYY", "YYYY MMMM DD"];
-  const parsed = dayjs(date, formats, "km");
+  const parsed = dayjs(date, formats, "km", true);
   const result = parsed.format("DD MMMM YYYY h:mm A");
-  console.log("result", result);
+
+  const test = dayjs("1970 មករា 04", "YYYY MMMM DD", "km", true).isValid(); // false
+  console.log("test", test);
 
   return (
     <>
